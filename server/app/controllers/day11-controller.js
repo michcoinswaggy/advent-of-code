c0ffad68f4c263b31e03b58fe159cf3ecc9fd4a6
michcoinswaggy <mich.cressant@gmail.com>
2020-12-15 18:38:23 +0100

    Day Eleven part two

--- server/app/controllers/day11-controller.js
+++ server/app/controllers/day11-controller.js
@@ -13,17 +13,18 @@ exports.getPartOne = (req, res, next) => {
         if (last) {
             let hasChanged = true;
             while (hasChanged) {
-                [hasChanged, grid] = getNewGrid(grid);
+                [hasChanged, grid] = getNewGrid(grid, false);
             }
-            let finalOccupiedSeats = countOccupiedSeats(grid);
+            let finalOccupiedSeats = countOccupiedSeats(grid); // 2319
             return res.status(200).json({ message: finalOccupiedSeats });
         }
     });
 }
 
-function getNewGrid(grid) {
+function getNewGrid(grid, isPartTwo) {
     let newGrid = [];
     let hasChanged = false;
+    let minOccupiedSeatToSwitchToEmpty = isPartTwo ? 5 : 4;
     for (let i = 0; i < grid.length ; i++) {
         let line = grid[i];
         let newLine = grid[i];
@@ -34,12 +35,12 @@ function getNewGrid(grid) {
             }
 
             if (position === "L") {
-                if (countAdjacentType(grid, i, j, "#") === 0) {
+                if (countAdjacentType(grid, i, j, "#", isPartTwo) === 0) {
                     newLine = replaceAt(newLine, j, "#");
                     hasChanged = true;
                 }
             } else if (position === "#") {
-                if (countAdjacentType(grid, i, j, "#")  >= 4) {
+                if (countAdjacentType(grid, i, j, "#", isPartTwo)  >= minOccupiedSeatToSwitchToEmpty) {
                     newLine = replaceAt(newLine, j, "L");
                     hasChanged = true;
                 }
@@ -52,25 +53,47 @@ function getNewGrid(grid) {
 }
 
 
-function countAdjacentType(grid, originX, originY, type) {
+function countAdjacentType(grid, originX, originY, type, isPartTwo) {
     let countAdjacentType = 0;
 
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX - 1, originY - 1) === type ? 1 : 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX - 1, originY) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX - 1, originY + 1) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX, originY - 1) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX, originY + 1) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX + 1, originY - 1) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX + 1, originY) === type ? 1: 0;
-    countAdjacentType += getAdjacentSeat(grid, originX, originY, originX + 1, originY + 1) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX - 1, originY - 1) === type ? 1 : 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX - 1, originY) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX - 1, originY + 1) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX, originY - 1) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX, originY + 1) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX + 1, originY - 1) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX + 1, originY) === type ? 1: 0;
+    countAdjacentType += getAdjacentSeat(grid, isPartTwo, originX, originY, originX + 1, originY + 1) === type ? 1: 0;
 
     return countAdjacentType;
 }
 
 
-function getAdjacentSeat(grid, originX, originY, indexX, indexY) {
+function getAdjacentSeat(grid, isPartTwo, originX, originY, indexX, indexY) {
     if (indexX < 0 || indexX > grid.length - 1 || indexY < 0 || indexY > grid[originX].length) {
         return false;
+    }
+
+    let adjacentSeat = grid[indexX].charAt(indexY);
+    if (!isPartTwo) {
+        return adjacentSeat;
+    }
+
+    // Seulement pour la partie 2
+    if (adjacentSeat === ".") {
+        if (indexX < originX) {
+            indexX--;
+        } else if (indexX > originX) {
+            indexX++;
+        }
+
+        if (indexY < originY) {
+            indexY--;
+        } else if (indexY > originY) {
+            indexY++;
+        }
+
+        return getAdjacentSeat(grid, isPartTwo, originX, originY, indexX, indexY);
     }
 
     return grid[indexX].charAt(indexY);
@@ -89,11 +112,16 @@ function countOccupiedSeats(grid) {
 }
 
 exports.getPartTwo = (req, res, next) => {
-    let grid = [];
+    let grid = []
     lineReader.eachLine('./inputs/day11/day11.txt', function(line, last) {
         grid.push(line);
-        if (last) {
-            return res.status(200).json({ message: "TODO" });
+        if (last) {
+            let hasChanged = true;
+            while (hasChanged) {
+                [hasChanged, grid] = getNewGrid(grid, true);
+            }
+            let finalOccupiedSeats = countOccupiedSeats(grid); // 2117
+            return res.status(200).json({ message: finalOccupiedSeats });
         }
     });
 }
